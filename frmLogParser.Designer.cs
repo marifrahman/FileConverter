8bf2275ee706a1016e16208be4d2792f0d9ceee9
marifrahman <sun2moon@gmail.com>
2013-04-27 14:34:48 +0930

    First Commit: Parsing a transaction file

---
+++ frmLogParser.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace BSSReport
+{
+    partial class frmLogParser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.tempLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Enabled = false;
+            this.txtLocation.Location = new System.Drawing.Point(84, 13);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(814, 20);
+            this.txtLocation.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 14);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File location";
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(903, 10);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(129, 25);
+            this.btnSelectFolder.TabIndex = 4;
+            this.btnSelectFolder.Text = "Select the log file";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "sadsdf";
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 4;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1267, 500);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Enabled = false;
+            this.btnExport.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(1040, 10);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 25);
+            this.btnExport.TabIndex = 9;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.AutoSize = true;
+            this.tempLabel.BackColor = System.Drawing.Color.GreenYellow;
+            this.tempLabel.Font = new System.Drawing.Font("Georgia", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempLabel.Location = new System.Drawing.Point(0, 0);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(0, 27);
+            this.tempLabel.TabIndex = 10;
+            this.tempLabel.Visible = false;
+            // 
+            // frmLogParser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 546);
+            this.Controls.Add(this.tempLabel);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmLogParser";
+            this.Text = "File Converter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label tempLabel;
+    }
+}
+
---
+++ frmLogParser.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace BSSReport
+{
+    public partial class frmLogParser : Form
+    {
+
+        string btslistdir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "adjcelldata\\");//Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "adjcelldata\\");
+        
+        public frmLogParser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtLocation.Text = ofd.FileName;
+                    string filePath = txtLocation.Text.ToString();
+                    List<string[]> parseData = parseFile(filePath);
+                    
+                    //DataTable refdt = loadBTSRef();
+
+                    DataTable newTable = new DataTable();
+
+                    newTable.Columns.Add(CreateDataColumn("System.String", "amnt", "Amount.", false, false, false));
+                    newTable.Columns.Add(CreateDataColumn("System.String", "acountno", "Accno", false, false, false));
+                    
+                    
+                    foreach (string[] line in parseData)
+                    {
+                        
+                            DataRow workRow = newTable.NewRow();
+                            workRow["amnt"] = line[0];
+                            workRow["acountno"] = line[1];
+
+                        newTable.Rows.Add(workRow);
+
+                    }
+
+                    dataGridView1.DataSource = newTable;//bindingSource;// 
+                    btnExport.Enabled = true;
+                }
+            }
+            catch (Exception xcp)
+            {
+                MessageBox.Show("An issue raised :" + xcp.Message.ToString());
+            }
+        }
+
+   
+        System.Data.DataColumn CreateDataColumn(string colType, string name, string caption, bool autoInc, bool readOnly, bool unique)
+        {
+
+
+            DataColumn column = new DataColumn();
+            column.DataType = System.Type.GetType(colType);
+            column.ColumnName = name;
+            column.Caption = caption;
+            column.AutoIncrement = autoInc;
+            column.AutoIncrementSeed = 1;
+            column.ReadOnly = readOnly;
+            column.Unique = unique;
+            return column;
+        }
+
+        private DataTable loadBTSRef()
+        {
+           
+            DataTable dt = new DataTable();
+            string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + btslistdir + "" +
+                   @";Extended Properties=""Text;HDR=yes;FMT=Delimited(,)\""";
+            try
+            {
+            OleDbConnection conn = new OleDbConnection(conString);
+            OleDbDataAdapter da = new OleDbDataAdapter(@"Select * from btsreftable.csv", conn);
+            
+                da.Fill(dt);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Problem loading the BTS Ref info:" + exp.Message.ToString());
+            }
+            return dt;
+        }
+
+        private List<string[]> parseFile(string path)
+        {
+            List<string[]> parsedData = new List<string[]>();
+
+            using (StreamReader readFile = new StreamReader(path))
+            {
+                string line;
+                string[] row;
+                string amnt = "";
+                string accno = "";
+                while ((line = readFile.ReadLine()) != null)
+                {
+                    if (line.IndexOf("16,699") != -1 )
+                    {
+                        
+                        
+                        if (line.Split(new Char[] { ',' })[4] == "020")
+                        {
+                            amnt = line.Split(new Char[] { ',' })[2];
+                            accno = line.Split(new Char[] { ',' })[5];
+                            row = new string[] { amnt, accno };
+                            parsedData.Add(row);
+
+                        }
+                        
+                     }
+
+                }
+            }
+
+            return parsedData;
+        }
+
+        
+        
+        Timer timer = new Timer();
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            Stream reportStream; 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "ANSI DOS(*.brs)|*.brs"; //Excel files (*.xlsx)|*.xlsx|txt files (*.txt)|*.txt|All files (*.*)|*.*
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((reportStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            saveToBRS(reportStream);
+                            break;
+                        //case 2:
+                        //    //saveToExcell(reportStream);
+                        //    saveToCSV(reportStream);
+                        //    break;
+                        //default:
+                        //    saveToCSV(reportStream);
+                        //    break;
+
+                    }
+                    
+                    reportStream.Close();
+                    showSuccessMsg("Successfuly exported the file :" + saveFileDialog1.FileName);
+                    
+                    
+
+                }
+            }
+        }
+        void showSuccessMsg(string msg)
+        {
+            dataGridView1.Enabled = false;
+            tempLabel.Location = new Point(430, 300);
+            tempLabel.Height = 200;
+            tempLabel.Width = 350;
+            tempLabel.Visible = true;
+            tempLabel.Text = msg;
+            timer.Tick += new EventHandler(timer_Tick); // Everytime timer ticks, timer_Tick will be called
+            timer.Interval = (1000) * (2);             // Timer will tick evert 10 seconds
+            timer.Enabled = true;                       // Enable the timer
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            tempLabel.Text = "";
+            tempLabel.Location = new Point(0, 0);
+            tempLabel.Height = 0;
+            tempLabel.Width = 0;
+            tempLabel.Visible = false;
+            timer.Stop();
+            timer.Enabled = false;
+            dataGridView1.Enabled = true;
+        }
+        void saveToCSV(Stream filename)
+        {
+            using (StreamWriter myFile = new StreamWriter(filename))
+            {
+                // Export titles:
+                string sHeaders = "";
+                for (int j = 0; j < dataGridView1.Columns.Count; j++) 
+                { 
+                    sHeaders = sHeaders.ToString() + Convert.ToString(dataGridView1.Columns[j].HeaderText) + ","; 
+                }
+                myFile.WriteLine(sHeaders);
+                // Export data.
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    string stLine = "";
+                    for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++) 
+                    { 
+                        stLine = stLine.ToString() + Convert.ToString(dataGridView1.Rows[i].Cells[j].Value) + ","; 
+                    }
+                    myFile.WriteLine(stLine);
+                }
+            }
+        }
+
+
+        void saveToBRS(Stream filename)
+        {
+            using (StreamWriter myFile = new StreamWriter(filename))
+            {
+                // Export titles:
+                string sHeaders = "#BRS#00000"+string.Format("yyyymmdd",DateTime.Now);
+                //for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                //{
+                //    sHeaders = sHeaders.ToString() + Convert.ToString(dataGridView1.Columns[j].HeaderText) + ",";
+                //}
+                myFile.WriteLine(sHeaders);
+                myFile.WriteLine("Somethign");
+                
+                int amount = 0;
+
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+
+                    amount += int.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    
+                    //string stLine = "";
+                    
+                    //for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+                    //{
+                    //    stLine = stLine.ToString() + Convert.ToString(dataGridView1.Rows[i].Cells[j].Value) + ",";
+                    //}
+                    
+                    
+                }
+
+
+                myFile.WriteLine("Some thing"+" "+"0500000000000"+amount);
+            }
+        }
+
+
+    }
+ }
